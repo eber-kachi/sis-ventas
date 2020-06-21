2d14ec4cd4c6723bef0ca5baac9a57672ca67bc0
playerblade <evotar156@gmail.com>
2020-06-21 17:03:22 -0400

    Agregado de  7 migraciones de las tablas sub_categories,categories,sends,stores,products,roles. y modificacion en la tabla de users.

---
+++ database/migrations/2020_06_21_205404_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('store_id');
+            $table->unsignedBigInteger('sub_category_id');
+            $table->string('title')->unique();
+            $table->integer('price');
+            $table->string('description',500);
+            $table->integer('stock');
+            $table->foreign('store_id')->references('id')->on('stores')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('sub_category_id')->references('id')->on('sub_categories')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
